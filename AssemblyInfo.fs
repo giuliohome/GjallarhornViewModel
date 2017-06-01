b8a9185bed540c77cfdf9a3ee4f6c8595a4963f2
Giulio <giuliohome@gmail.com>
2017-06-01 10:38:43 +0200

    my first example of functional Gjallarhorn ViewModel (based on Signal, Observable etc...) for a Button command writing to a TextBox

---
+++ AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace UFuncSimpleTest.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("UFuncSimpleTest")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("MDT Solution Pack")>]
+[<assembly: AssemblyProduct("UFuncSimpleTest")>]
+[<assembly: AssemblyCopyright("Copyright © MDT Solution Pack 2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("bfe3ce67-22e4-43a3-9159-ca85b7b9437d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Context.fs
@@ -0,0 +1,35 @@
+﻿module Context
+
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Validation
+open Gjallarhorn.Validation.Validators
+
+
+let create () =    
+    // Create our binding source
+    let source = Binding.createSource ()
+
+    // Create two mutable values from our initial values
+    let result = Mutable.create "hello world"
+
+    let canExecute =  Mutable.create true
+
+    // Display the results bound as "Result"
+    Binding.toView source "Result" result
+
+    let okCommand = source |> Binding.createCommandChecked "ChangeCommand" canExecute
+
+    let okLogic(_) = 
+        canExecute.Value <- false
+        async{
+        result.Value <- result.Value + " ! " 
+        do!  Async.Sleep 3000 
+        canExecute.Value <- true
+        } |> Async.StartImmediate
+
+    okCommand 
+    |> Observable.subscribe(okLogic)
+    |> source.AddDisposable
+
+    source
